7a8a46d98401cb00e7460c625edf1d922838cc4c
jbracher <jbracher@outlook.de>
2020-05-02 16:01:07 +0200

    New version of plots showing SARIMA and KCDE

--- comparison_flusight/plot_kcde_sarima.R
+++ comparison_flusight/plot_kcde_sarima.R
@@ -1,3 +1,6 @@
+# Generate figure illustrating interval scores for KCDE and SARIMA
+# Johannes Bracher, May 2020, johannes.bracher@kit.edu
+
 source("comparison_flusight/functions_flusight.R")
 
 
@@ -21,58 +24,139 @@ sarima <- get_forecasts(model = "ReichLab_sarima_seasonal_difference_TRUE",
                       season = "2016/2017")
 
 # perform scoring:
-scores_kcde <- score_forecasts(kcde, miS.alpha = 0.2)
-scores_sarima <- score_forecasts(sarima, miS.alpha = 0.2)
+scores_kcde_0.2 <- averaged_interval_score_table(kcde, alpha = c(0.2), detailed = TRUE)
+scores_sarima_0.2 <- averaged_interval_score_table(sarima, alpha = c(0.2), detailed = TRUE)
+
+scores_kcde_0.2_1 <- averaged_interval_score_table(kcde, alpha = c(0.2, 1), detailed = TRUE)
+scores_sarima_0.2_1 <- averaged_interval_score_table(sarima, alpha = c(0.2, 1), detailed = TRUE)
+
+scores_kcde_detailed <- averaged_interval_score_table(kcde, alpha = c(0.02, 0.05, 1:9/10), detailed = TRUE)
+scores_sarima_detailed <- averaged_interval_score_table(sarima, alpha = c(0.02, 0.05, 1:9/10), detailed = TRUE)
 
 
-# plotting function:
-plot_mis <- function(fc, ylim = c(0, max(fc$miS)), ylab = "MIS"){
-  plot(fc$miS.abs_error + fc$miS.width_pi + fc$miS.penalty,
+# plotting functions:
+plot_ais_0.2_1 <- function(fc, ylim = c(0, max(fc$ais)), ylab = expression(AIS[list(0, 0.2)]), ...){
+  plot(0.5*(fc$penalty.alpha.1 + fc$penalty.alpha.0.2 + fc$width_pi.alpha.0.2),
        xlab = "week of season", lwd = 2, type = "h",
-       col = "red", ylim = ylim, ylab = ylab)
-  points(fc$miS.width_pi + fc$miS.abs_error, lwd = 2, type = "h", col = "orange")
-  points(fc$miS.width_pi, lwd = 2, type = "h", col = "blue")
+       col = "red", ylim = ylim, ylab = ylab, ...)
+  points(0.5*(fc$width_pi.alpha.0.2 + fc$penalty.alpha.1), lwd = 2, type = "h", col = "lightcoral")
+  points(0.5*(fc$width_pi.alpha.0.2), lwd = 2, type = "h", col = "royalblue1")
+  abline(h = 0)
+}
+
+plot_ais <- function(fc, ylim = c(0, max(fc$ais)), ylab = "AIS", ...){
+  plot(0.5*(fc$averaged_interval_score),
+       xlab = "week of season", lwd = 2, type = "h",
+       col = "red", ylim = ylim, ylab = ylab, ...)
+  points(0.5*(fc$averaged_width_pi), lwd = 2, type = "h", col = "royalblue1")
   abline(h = 0)
 }
 
 # make plot:
-pdf("plot_kcde_sarima.pdf", width = 8, height = 4)
+pdf("comparison_flusight/plot_kcde_sarima.pdf", width = 8.5, height = 6)
 
-par(mfrow = c(2, 2), mar = c(4, 4, 2, 2), las = 1)
-layout(matrix(c(rep(1, 3), rep(2, 3), rep(5, 2),
-                rep(3, 3), rep(4, 3), rep(5, 2)), nrow = 2, byrow = TRUE))
-
-plot(kcde$truth, pch = 16, xlab = "week of season", ylab = "%wILI", ylim = c(0, 8),
-     main = "KCDE")
-lines(scores_kcde$miS.m, lty = 2)
-lines(scores_kcde$miS.l, lty = 3)
-lines(scores_kcde$miS.u, lty = 3)
-
-legend("topleft", legend = c("median", "80% PI", "observed"),
-       lty = c(2, 3, NA), pch = c(NA, NA, 15), bty = "n")
+par(mfrow = c(2, 2), mar = c(4, 4.5, 2, 2), las = 1)
+layout(matrix(c(rep(1, 3), rep(2, 3), rep(3, 2),
+                rep(1, 3), rep(2, 3), rep(3, 2),
+                rep(4, 3), rep(5, 3), rep(3, 2),
+                rep(4, 3), rep(5, 3), rep(8, 2),
+                rep(6, 3), rep(7, 3), rep(8, 2),
+                rep(6, 3), rep(7, 3), rep(8, 2)), nrow = 6, byrow = TRUE))
 
 
-plot(sarima$truth, pch = 16, xlab = "week of season", ylab = "%wILI", ylim = c(0, 8),
-     main = "SARIMA")
-lines(scores_sarima$miS.m, lty = 2)
-lines(scores_sarima$miS.l, lty = 3)
-lines(scores_sarima$miS.u, lty = 3)
+# #------------------------------------
+# # Plot for AIS with alpha = c(0, 0.2)
+# plot_ais_0.2_1(scores_kcde_0.2_1, ylim = c(0, 5), main = "KCDE")
+# plot_ais_0.2_1(scores_sarima_0.2_1, ylim = c(0, 5), main = "SARIMA")
+#
+# plot(c(mean(scores_kcde_0.2_1$averaged_interval_score), mean(scores_sarima_0.2_1$averaged_interval_score)), type = "h",
+#      ylim = c(0, 3), xlim = c(0.5, 2.5), axes = FALSE,
+#      ylab = expression(average~AIS[list(0, 0.2)]),
+#      col = "red", lwd = 2, xlab = "")
+# axis(1, at = 1:2, labels = c("KCDE", "SARIMA"), cex.axis = 1)
+# axis(2); box()
+# lines(c(0.5*mean(scores_kcde_0.2_1$width_pi.alpha.0.2 + scores_kcde_0.2_1$penalty.alpha.1),
+#         0.5*mean(scores_sarima_0.2_1$width_pi.alpha.0.2 + scores_sarima_0.2_1$penalty.alpha.1)),
+#       col = "lightcoral", lwd = 2, type = "h")
+# lines(c(0.5*mean(scores_kcde_0.2_1$width_pi.alpha.0.2),
+#         0.5*mean(scores_sarima_0.2_1$width_pi.alpha.0.2)),
+#       col = "royalblue1", lwd = 2, type = "h")
+# legend("top", legend = c("average penalty for\n non-coverage\n of 80% PI", "MAE/2",
+#                          "average half-width\n of 80% PI"),
+#        col = c("red", "lightcoral", "royalblue1"), lwd = 2, bty = "n", cex = 1)
 
-plot_mis(scores_kcde, ylim = c(0, 16), ylab = expression(MIS[0.2]))
-plot_mis(scores_sarima, ylim = c(0, 16), ylab = expression(MIS[0.2]))
 
-plot(c(mean(scores_kcde$miS), mean(scores_sarima$miS)), type = "h",
-     ylim = c(0, 3), xlim = c(0.5, 2.5), axes = FALSE,
-     ylab = expression(average~MIS[0.2]),
+
+plot_ais(scores_kcde_0.2, ylim = c(0, 3), ylab = expression(IS[0.2]))
+legend("top", legend = c("penalty for non-coverage of 80% PI", "width of 80% PI"),
+       col = c("red", "royalblue1"), lwd = 2, bty = "n", cex = 1)
+plot_ais(scores_sarima_0.2, ylim = c(0, 3), ylab = expression(IS[0.2]))
+
+plot(c(mean(scores_kcde_0.2$averaged_interval_score), mean(scores_sarima_0.2$averaged_interval_score)), type = "h",
+     ylim = c(0, 1.5), xlim = c(0.5, 2.5), axes = FALSE,
+     ylab = expression(average~IS[0.2]),
      col = "red", lwd = 2, xlab = "")
 axis(1, at = 1:2, labels = c("KCDE", "SARIMA"), cex.axis = 1)
 axis(2); box()
-lines(c(mean(scores_kcde$miS.abs_error + scores_kcde$miS.width_pi),
-        mean(scores_sarima$miS.abs_error + scores_sarima$miS.width_pi)),
-      col = "orange", lwd = 2, type = "h")
-lines(c(mean(scores_kcde$miS.width_pi),
-        mean(scores_sarima$miS.width_pi)),
-      col = "blue", lwd = 2, type = "h")
-legend("top", legend = c("penalty for\n non-coverage", "absolute error", "width of PI"),
-       col = c("red", "orange", "blue"), lwd = 2, bty = "n", cex = 1)
+lines(c(0.5*mean(scores_kcde_0.2$averaged_width_pi),
+        0.5*mean(scores_sarima_0.2$averaged_width_pi)),
+      col = "royalblue1", lwd = 2, type = "h")
+# legend("top", legend = c("penalty for\n non-coverage\n of 80% PI", "average width\n of 80% PI"),
+#        col = c("red", "lightcoral", "royalblue1"), lwd = 2, bty = "n", cex = 1)
+
+
+#--------------------------------------
+# time series plot
+cex.p <- 0.8
+
+add_pi <- function(l, u, col = "royalblue1"){
+  polygon(c(seq_along(l), rev(seq_along(u))),
+          c(l, rev(u)), col = col, border = col)
+}
+
+plot(kcde$truth, pch = 16, xlab = "week of season", ylab = "%wILI", ylim = c(0, 8),
+     main = "", cex = cex.p)
+add_pi(scores_kcde_0.2_1$l.alpha.0.2, scores_kcde_0.2_1$u.alpha.0.2)
+points(kcde$truth, pch = 16, cex = cex.p)
+lines(scores_kcde_0.2_1$u.alpha.1, lty = 3)
+
+
+legend("topleft", legend = c("median", "80% PI", "observed"),
+       lty = c(2, 1, NA), pch = c(NA, NA, 15), bty = "n",
+       col = c("black", "royalblue1", "black"), lwd = c(1, 4, 1))
+
+plot(sarima$truth, pch = 16, xlab = "week of season", ylab = "%wILI", ylim = c(0, 8),
+     main = "", cex = cex.p)
+add_pi(scores_sarima_0.2_1$l.alpha.0.2, scores_sarima_0.2_1$u.alpha.0.2)
+points(sarima$truth, pch = 16, cex = cex.p)
+lines(scores_sarima_0.2_1$u.alpha.1, lty = 3)
+
+#------------------------------------
+# Plot for AIS with alpha = c(0.02, 0.05, 0.1, ..., 0.9)
+
+
+
+
+plot_ais(scores_kcde_detailed, ylim = c(0, 3), expression(AIS[detailed]))
+legend("top", legend = c("average penalty for non-coverage", "average width of PIs"),
+       col = c("red", "royalblue1"), lwd = 2, bty = "n", cex = 1)
+
+plot_ais(scores_sarima_detailed, ylim = c(0, 3), expression(AIS[detailed]))
+
+plot(c(mean(scores_kcde_detailed$averaged_interval_score), mean(scores_sarima_detailed$averaged_interval_score)), type = "h",
+     ylim = c(0, 1.5), xlim = c(0.5, 2.5), axes = FALSE,
+     ylab = expression(average~AIS[detailed]),
+     col = "red", lwd = 2, xlab = "")
+axis(1, at = 1:2, labels = c("KCDE", "SARIMA"), cex.axis = 1)
+axis(2); box()
+lines(c(0.5*mean(scores_kcde_detailed$averaged_width_pi),
+        0.5*mean(scores_sarima_detailed$averaged_width_pi)),
+      col = "royalblue1", lwd = 2, type = "h")
+# legend("top", legend = c("average penalty for\n non-coverage", "average width\n of PIs"),
+#        col = c("red", "royalblue1"), lwd = 2, bty = "n", cex = 1)
 dev.off()
+
+# numbers for text:
+
+1 - mean(scores_sarima_0.2$penalty.alpha.0.2 > 0)
+1 - mean(scores_kcde_0.2$penalty.alpha.0.2 > 0)
